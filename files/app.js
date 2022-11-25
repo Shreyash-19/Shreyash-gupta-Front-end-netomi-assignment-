f55281a8e0d84bef50f9df6ba1e97a4d8d153fb8
Shreyash Gupta <63347527+Shreyash-19@users.noreply.github.com>
2022-11-25 17:16:25 +0530

    Add files via upload

---
+++ files/app.js
@@ -0,0 +1,5 @@
+window.addEventListener('message', function (e) {
+    const data = JSON.parse(e.data);
+    console.log(data);
+    document.getElementById('shows-result').innerHTML = `Result:${data.message}`;
+});
